ec30eed40d7fac76f64736bbf740a2dbc54faf2a
cristopher <crisotopher.saraos@gmail.com>
2021-12-02 15:23:45 -0300

    install SweetAlert2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,9 @@ import { GraphComponent } from './page/graph/graph.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SweetAlert2Module.forChild(),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
