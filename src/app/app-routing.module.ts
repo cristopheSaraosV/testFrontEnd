0b18689cec201a8b78675a8669465f8c3f1d6ead
cristopher <crisotopher.saraos@gmail.com>
2021-12-02 15:15:39 -0300

    create routing home and graph

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { GraphComponent } from './page/graph/graph.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:HomeComponent },
+  { path:'graph', component: GraphComponent},
+  { path:'**', redirectTo:'' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
